8a26b2cae6952f4fe137abae8b581e4860e7f9a1
Satnam Singh <satnam.singh156@t-mobile.com>
2020-05-21 00:59:07 +0530

    update

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
 await fs.ensureDir('build')
-await fs.copyFile('./payment-app/payment/main.js', 'build/paymentelement.js');
+await fs.copyFile('./payment-app/elements/main.js', 'build/paymentelement.js');
 await fs.copyFile('./header-app/elements/main.js', 'build/headerelement.js');
 await fs.copyFile('./react-app/build/static/js/bundle.min.js', 'build/react.js');
 })()
